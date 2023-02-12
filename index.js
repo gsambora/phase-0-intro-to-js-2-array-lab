74c03b634404517794d986ee82fe90484070aa98
Hana <grace.sambora@gmail.com>
2023-02-12 00:54:39 -0600

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Write your solution here!
+
+var cats = ["Milo", "Otis", "Garfield"];
+console.log(cats)
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+function appendCat(name) {
+    var newcats = [...cats, name];
+    console.log(newcats);
+    return newcats;
+}
+
+function prependCat(name) {
+    var newcats = [name, ...cats];
+    return newcats;
+}
+
+function removeLastCat(){
+    var newcats = cats.slice(0,-1);
+    console.log(newcats);
+    return newcats;
+}
+
+function removeFirstCat(){
+    var newcats = cats.slice(1);
+    return newcats;
+}
